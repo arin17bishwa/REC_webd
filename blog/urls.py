3ce1f89981c92b815eeb22143fcd99c2666e27f4
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-11-13 01:29:00 +0530

    just added some garbage

--- blog/urls.py
+++ blog/urls.py
@@ -16,3 +16,62 @@ urlpatterns = [
     path('<slug>/delete', delete_blog_view, name='delete'),
     path('react', action_view, name='react'),
 ]
+'''
+
+
+
+
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #f2f2f2;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add an active class to highlight the current page */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+/* Hide the link that should open and close the topnav on small screens */
+.topnav .icon {
+  display: none;
+}
+
+'''
+
+
+
+'''
+        <div class="date-filter">
+            <form method="get">{% csrf_token %}
+                <div style="float: left">
+                    <label for="id_start">Start</label>
+                    <input class="form-control" type="date" name="start" id="id_start" placeholder="Start Date" required autofocus/>
+                </div>
+                <div style="float: right">
+                    <label for="id_end">End</label>
+                    <input class="form-control" type="date" name="end" id="id_end" placeholder="End Date" required autofocus/>
+                </div>
+                <button type="submit">Filter</button>
+            </form>
+        </div>
+
+'''
