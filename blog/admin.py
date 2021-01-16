9159d0de61bc3acf9175c8048bb9d1c6aec2c80b
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-01-16 16:45:03 +0530

    registered Comment model

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from blog.models import BlogPost
+from .models import BlogPost,Comment
 
-admin.site.register(BlogPost)
+admin.site.register((BlogPost,Comment))
